1ac4e89e25fdc13e1aa86a93429f18101a45aff0
unknown <neomarcaria@gmail.com>
2019-12-23 22:19:28 +0900

    被写体をDBに新規登録

---
+++ registerObject.js
@@ -0,0 +1,27 @@
+// JavaScript File
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+var dynamo = new AWS.DynamoDB.DocumentClient({
+  region: 'ap-northeast-1'
+});
+
+exports.handler = (event, context, callback) => {
+
+  var params = {
+    TableName: 'Target',
+    Item: {
+      'target_name': 'Ž‡—z‰Ô',
+      'target_id': 3,
+    }
+  };
+
+  dynamo.put(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    }
+    else {
+      console.log("Success", data);
+    }
+  });
+};
