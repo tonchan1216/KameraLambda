a71d5b2f9cb2b516101dfc285a24f6b000ac787d
tonchan <cleartone1216@gmail.com>
2020-01-13 16:55:25 +0900

    add getSubject

---
+++ getSubjects/index.js
@@ -0,0 +1,26 @@
+var AWS = require('aws-sdk');
+var dynamo = new AWS.DynamoDB.DocumentClient({
+  region: 'ap-northeast-1'
+});
+
+exports.handler = (event, context, callback) => {
+  var params = {
+    TableName: "subject",
+  };
+  var response = {
+    "headers": {},
+    "isBase64Encoded": false
+  };
+
+  dynamo.scan(params, function (err, data) {
+    if (err) {
+      console.log(err);
+      response.statusCode = 400;
+      response.body = JSON.stringify(err);
+    } else {
+      response.statusCode = 200;
+      response.body = JSON.stringify(data);
+    }
+    context.done(null, response);
+  });
+};
